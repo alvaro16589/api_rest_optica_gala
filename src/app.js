9aa237447bcb52740cc8d36f6cf39a931bf8b913
alvaro <54456217+alvaro16589@users.noreply.github.com>
2023-02-12 21:03:58 -0400

    solucionando cors police permisos de coneccion

--- src/app.js
+++ src/app.js
@@ -7,12 +7,18 @@ import recetRoutes from './routes/recet.routes.js';
 const app = express();
 app.use(express.json())//convert  body to jSon sentence
 const pref = '/api';
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 //connect to routes
 app.use(pref, customerRoutes);
 app.use(pref, telephoneRoutes);
 app.use(pref, recetRoutes);
 //middlewere
+
 app.unsubscribe((req,res,next)=>{
     res.status(400).json({
         message : 'Route not found'
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { dotEnvValues } from "./config.js";
 import app from "./app.js";
 
 
-app.listen(3000 );  
+app.listen(3000);  
 console.log('server running on port : ', dotEnvValues.PORT)
